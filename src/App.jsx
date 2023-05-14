271185ead5cd9d38dda3d7b6e5dffe69eab5cc6a
SouravKAgarwal <aggarwalsaurav948@gmail.com>
2023-05-14 12:40:45 +0530

    Header updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
-import { useSelector, useDispatch } from 'react-redux';
-import { getApiConfiguration, getGenres } from './store/homeSlice';
+
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
@@ -12,59 +13,58 @@ import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
 import PageNotFound from "./pages/404/PageNotFound";
 
-
 function App() {
-  const dispatch = useDispatch();
-  const { url } = useSelector((state) => state.home);
-  useEffect(() => {
-    fetchApiConfig();
-    genresCall();
-  }, []);
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
 
-  const fetchApiConfig = () => {
-    fetchDataFromApi("/configuration")?.then(res => {
+    useEffect(() => {
+        fetchApiConfig();
+        genresCall();
+    }, []);
 
-      const url = {
-        backdrop: res.images.secure_base_url + "original",
-        poster: res.images.secure_base_url + "original",
-        profile: res.images.secure_base_url + "original",
-      }
-      dispatch(getApiConfiguration(url))
-    })
-  };
+    const fetchApiConfig = () => {
+        fetchDataFromApi("/configuration").then((res) => {
 
+            const url = {
+                backdrop: res?.images?.secure_base_url + "original",
+                poster: res?.images?.secure_base_url + "original",
+                profile: res?.images?.secure_base_url + "original",
+            };
 
-  const genresCall = async () => {
-    let promises = [];
-    let endPoints = ["tv", "movie"];
-    let allGenres = {};
+            dispatch(getApiConfiguration(url));
+        });
+    };
 
-    endPoints.forEach((url) => {
-        promises.push(fetchDataFromApi(`/genre/${url}/list`));
-    });
+    const genresCall = async () => {
+        let promises = [];
+        let endPoints = ["tv", "movie"];
+        let allGenres = {};
 
-    const data = await Promise.all(promises);
-    console.log(data);
-    data.map(({ genres }) => {
-        return genres.map((item) => (allGenres[item.id] = item));
-    });
+        endPoints.forEach((url) => {
+            promises.push(fetchDataFromApi(`/genre/${url}/list`));
+        });
 
-    dispatch(getGenres(allGenres));
-};
+        const data = await Promise.all(promises);
+        data?.map(({ genres }) => {
+            return genres?.map((item) => (allGenres[item.id] = item));
+        });
 
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:mediaType/:id" element={<Details />} />
-        <Route path="/search/:query" element={<SearchResult />} />
-        <Route path="/explore/:mediaType" element={<Explore />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  )
+        dispatch(getGenres(allGenres));
+    };
+
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:mediaType/:id" element={<Details />} />
+                <Route path="/search/:query" element={<SearchResult />} />
+                <Route path="/explore/:mediaType" element={<Explore />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -15,8 +15,6 @@ import CircleRating from "../circleRating/CircleRating";
 import "./style.scss";
 import Genres from "../genres/Genres";
 
-
-
 const Carousel = ({ data, loading, endpoint, title }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
@@ -24,14 +22,16 @@ const Carousel = ({ data, loading, endpoint, title }) => {
 
   const navigation = (dir) => {
     const container = carouselContainer.current;
-    const scrollAmount = dir === "left" ? container.scrollLeft - (container.offsetWidth + 20) : container.scrollLeft + (container.offsetWidth + 20)
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
 
     container.scrollTo({
       left: scrollAmount,
       behaviour: "smooth",
-    })
-  }
-
+    });
+  };
 
   const skItem = () => {
     return (
@@ -42,9 +42,8 @@ const Carousel = ({ data, loading, endpoint, title }) => {
           <div className="date skeleton"></div>
         </div>
       </div>
-    )
-  }
-
+    );
+  };
 
   return (
     <div className="carousel">
@@ -61,28 +60,31 @@ const Carousel = ({ data, loading, endpoint, title }) => {
         {!loading ? (
           <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
-              const posterUrl = item.poster_path ? url.poster + item.poster_path : PosterFallback
+              const posterUrl = item.poster_path
+                ? url.poster + item.poster_path
+                : PosterFallback;
               return (
                 <div
                   key={item.id}
-                  className="carouselItem" onClick={() => navigate(`/${item.media_type || endpoint}/${item.id}`)}>
+                  className="carouselItem"
+                  onClick={() =>
+                    navigate(`/${item.media_type || endpoint}/${item.id}`)
+                  }
+                >
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />
                     <Genres data={item.genre_ids.slice(0, 2)} />
                   </div>
                   <div className="textBlock">
-                    <span className="title">
-                      {item.title || item.name}
-                    </span>
+                    <span className="title">{item.title || item.name}</span>
                     <span className="date">
                       {dayjs(item.release_Date).format("MMM D, YYYY")}
                     </span>
                   </div>
                 </div>
-              )
+              );
             })}
-
           </div>
         ) : (
           <div className="loadingSkeleton">
@@ -95,7 +97,7 @@ const Carousel = ({ data, loading, endpoint, title }) => {
         )}
       </ContentWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
