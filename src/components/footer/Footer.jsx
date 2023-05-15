35e1e2df59ffb2b1bb2147cab7441d7170685b61
SouravKAgarwal <aggarwalsaurav948@gmail.com>
2023-05-16 01:46:05 +0530

    done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import {
-    FaFacebookF,
     FaInstagram,
-    FaTwitter,
     FaLinkedin,
     FaGithub,
 } from "react-icons/fa";
@@ -22,14 +20,6 @@ const Footer = () => {
                     <li className="menuItem">Blog</li>
                     <li className="menuItem">FAQ</li>
                 </ul>
-                {/* <div className="infoText">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur.
-                </div> */}
                 <div className="socialIcons">
                     <span className="icon">
                     <Link href="https://github.com/SouravKAgarwal" target='_blank'><FaGithub className='text-[white] text-[24px]'/></Link>
